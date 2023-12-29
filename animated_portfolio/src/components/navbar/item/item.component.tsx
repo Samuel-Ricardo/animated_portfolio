b643889ddf6f779bb0526e96741a1013e4ccf982
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 11:09:32 -0300

    [ :art: ] | update: component - navbar > item (app)

--- animated_portfolio/src/components/navbar/item/item.component.tsx
+++ animated_portfolio/src/components/navbar/item/item.component.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const NavbarItem = ({ link, icon, alt }: INavbarItemProps) => (
-  <Link href={link} className="link">
-    <Image src={icon} alt={alt} width={64} height={64} />
+  <Link href={link}>
+    <Image src={icon} alt={alt} width={18} height={18} />
   </Link>
 );
