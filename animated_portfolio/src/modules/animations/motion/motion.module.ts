901e71f34f6f603384ac97ee0cea7592c1dcdc8f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 15:48:26 -0300

    [ :clapper: ] | update: module - motion > text (modules::animation)

--- animated_portfolio/src/modules/animations/motion/motion.module.ts
+++ animated_portfolio/src/modules/animations/motion/motion.module.ts
@@ -2,9 +2,13 @@ import { Container } from "inversify";
 import { MOTION_REGISTRY } from "./motion.registry";
 import { fadeIn } from "./variations/fadein.animation";
 import getDecorators from "inversify-inject-decorators";
+import { defaultTextVariants } from "./variations/text.animation";
 
 export const MOTION_MODULE = new Container({ defaultScope: "Singleton" });
 
 MOTION_MODULE.bind(MOTION_REGISTRY.FADE.IN).toConstantValue(fadeIn);
+MOTION_MODULE.bind(MOTION_REGISTRY.TEXT.DEFAULT).toConstantValue(
+  defaultTextVariants,
+);
 
 export const { lazyInject: injectMotion } = getDecorators(MOTION_MODULE);
