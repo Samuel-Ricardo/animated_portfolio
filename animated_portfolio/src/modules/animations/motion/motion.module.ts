91d161f162e8c47ba49236f8d869121135eb7cb5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 09:17:34 -0300

    [ :syringe: ] | update: module - motion (modules::animation)

--- animated_portfolio/src/modules/animations/motion/motion.module.ts
+++ animated_portfolio/src/modules/animations/motion/motion.module.ts
@@ -1,7 +1,10 @@
 import { Container } from "inversify";
 import { MOTION_REGISTRY } from "./motion.registry";
 import { fadeIn } from "./variations/fadein.animation";
+import getDecorators from "inversify-inject-decorators";
 
 export const MOTION_MODULE = new Container({ defaultScope: "Singleton" });
 
 MOTION_MODULE.bind(MOTION_REGISTRY.FADE.IN).toConstantValue(fadeIn);
+
+export const { lazyInject: injectMotion } = getDecorators(MOTION_MODULE);
