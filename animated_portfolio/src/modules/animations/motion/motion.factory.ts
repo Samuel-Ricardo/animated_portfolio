5f461e6840e2782b8fb9760056ac5d0da80104fd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 15:49:09 -0300

    [ :clapper: ] | update: factory - motion > text (modules::animation)

--- animated_portfolio/src/modules/animations/motion/motion.factory.ts
+++ animated_portfolio/src/modules/animations/motion/motion.factory.ts
@@ -1,9 +1,16 @@
 import { MOTION_MODULE } from "./motion.module";
 import { MOTION_REGISTRY } from "./motion.registry";
 import { fadeIn } from "./variations/fadein.animation";
+import { defaultTextVariants } from "./variations/text.animation";
 
 export const MOTION_FACTORY = {
   FADE: {
     IN: () => MOTION_MODULE.get<typeof fadeIn>(MOTION_REGISTRY.FADE.IN),
+  },
+  TEXT: {
+    DEFAULT: () =>
+      MOTION_MODULE.get<typeof defaultTextVariants>(
+        MOTION_REGISTRY.TEXT.DEFAULT,
+      ),
   },
 };
