359d9801b2585efc2758ed965fc3119a237d165a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 15:22:51 -0300

    [ :art: ] | update: page - home (app)

--- animated_portfolio/src/app/page.tsx
+++ animated_portfolio/src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Cursor } from "@/components/cursor/cursor.component";
+
 export default function Home() {
   return (
     <main>
+      <Cursor />
       <section id="Homepage">Homepage</section>
       <section id="Services">Services</section>
       <section id="Portfolio">Portfolio</section>
