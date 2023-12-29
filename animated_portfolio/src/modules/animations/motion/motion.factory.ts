2688113fd1177335d418e8d9e877ff8f6e1d7c72
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 13:54:26 -0300

    [ :clapper: ] | create: factory - motion (modules::animation)

--- animated_portfolio/src/modules/animations/motion/motion.factory.ts
+++ animated_portfolio/src/modules/animations/motion/motion.factory.ts
@@ -1,8 +1,9 @@
 import { MOTION_MODULE } from "./motion.module";
 import { MOTION_REGISTRY } from "./motion.registry";
+import { fadeIn } from "./variations/fadein.animation";
 
 export const MOTION_FACTORY = {
   FADE: {
-    IN: () => MOTION_MODULE.get(MOTION_REGISTRY.FADE.IN),
+    IN: () => MOTION_MODULE.get<typeof fadeIn>(MOTION_REGISTRY.FADE.IN),
   },
 };
