0ecc4b59bf2d3139fea968614d9ddf0ea67de7bb
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 11:11:18 -0300

    [ :art: ] | update: component - navbar (app)

--- animated_portfolio/src/components/navbar/container/container.component.tsx
+++ animated_portfolio/src/components/navbar/container/container.component.tsx
@@ -2,13 +2,15 @@ import { NavbarItem } from "../item/item.component";
 import { NavbarTitle } from "../title/title.component";
 
 export const NavbarContainer = () => (
-  <nav className="navbar_container">
+  <nav className="navbar">
     <div className="wrapper">
       <NavbarTitle />
-      <NavbarItem link={"#"} icon={"/facebook.png"} alt="facebook icon" />
-      <NavbarItem link={"#"} icon={"/instagram.png"} alt="facebook icon" />
-      <NavbarItem link={"#"} icon={"/youtube.png"} alt="facebook icon" />
-      <NavbarItem link={"#"} icon={"/dribbble.png"} alt="facebook icon" />
+      <div className="social">
+        <NavbarItem link={"#"} icon={"/facebook.png"} alt="facebook icon" />
+        <NavbarItem link={"#"} icon={"/instagram.png"} alt="facebook icon" />
+        <NavbarItem link={"#"} icon={"/youtube.png"} alt="facebook icon" />
+        <NavbarItem link={"#"} icon={"/dribbble.png"} alt="facebook icon" />
+      </div>
     </div>
   </nav>
 );
