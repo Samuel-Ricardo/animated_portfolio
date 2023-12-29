2601e1a83addf9be4973aef24d42b1ad92daf2ae
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 10:10:59 -0300

    [ :art: ] | create: component - navbar > title (app)

---
+++ animated_portfolio/src/components/navbar/title/title.component.tsx
@@ -0,0 +1,9 @@
+import { INavbarTitleProps } from "@/@types/props/navbar/title";
+import { MotionH1 } from "@/components/motion/h1.component";
+import { MODULES } from "@/modules/app.factory";
+
+export const NavbarTitle = ({ title = "Samuel Barros" }: INavbarTitleProps) => {
+  const fadeIn = MODULES.ANIMATION.MOTION.FADE.IN();
+
+  return <MotionH1 {...fadeIn({})}>{title}</MotionH1>;
+};
