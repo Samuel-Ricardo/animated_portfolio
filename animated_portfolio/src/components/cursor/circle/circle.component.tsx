fdc4305c1f22f990982e4e20070c013b050b219c
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 15:19:45 -0300

    [ :art: ] | update: component - cursos > circle (app)

--- animated_portfolio/src/components/cursor/circle/circle.component.tsx
+++ animated_portfolio/src/components/cursor/circle/circle.component.tsx
@@ -1,6 +1,14 @@
+import "./circle.style.scss";
+
 import { ICursorCircleProps } from "@/@types/props/cursor/circle";
 import { MotionDiv } from "@/components/motion/div.component";
 
 export const Circle = ({ position }: ICursorCircleProps) => (
-  <MotionDiv className="circle" animate={{ ...position }} />
+  <MotionDiv
+    className="circle"
+    animate={{
+      x: position.x - 14,
+      y: position.y - 110,
+    }}
+  />
 );
