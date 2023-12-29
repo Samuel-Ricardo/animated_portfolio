24ec6dcc1a7ee667dbd1bf25967dd9b0e9b9c2db
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 15:20:02 -0300

    [ :art: ] | update: component - cursos (app)

--- animated_portfolio/src/components/cursor/cursor.component.tsx
+++ animated_portfolio/src/components/cursor/cursor.component.tsx
@@ -1,18 +1,5 @@
 "use client";
-
-import { useEffect, useState } from "react";
+import { useMousePosition } from "@/hooks/cursor.hook";
 import { Circle } from "./circle/circle.component";
 
-export const Cursor = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleMouseMove = (event: MouseEvent) =>
-      setPosition({ x: event.x, y: event.x });
-
-    window.addEventListener("mousemove", handleMouseMove);
-    return () => window.removeEventListener("mousemove", handleMouseMove);
-  }, []);
-
-  return <Circle position={position} />;
-};
+export const Cursor = () => <Circle position={useMousePosition()} />;
