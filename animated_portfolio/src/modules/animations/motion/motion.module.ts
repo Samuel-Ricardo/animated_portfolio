e3066d12e0eceae30d117b3a34a75a0ec074e878
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 15:55:05 -0300

    [ :clapper: ] | update: module - motion > slider (modules::animation)

--- animated_portfolio/src/modules/animations/motion/motion.module.ts
+++ animated_portfolio/src/modules/animations/motion/motion.module.ts
@@ -3,6 +3,7 @@ import { MOTION_REGISTRY } from "./motion.registry";
 import { fadeIn } from "./variations/fadein.animation";
 import getDecorators from "inversify-inject-decorators";
 import { defaultTextVariants } from "./variations/text.animation";
+import { slider } from "./variations/slider.animation";
 
 export const MOTION_MODULE = new Container({ defaultScope: "Singleton" });
 
@@ -10,5 +11,6 @@ MOTION_MODULE.bind(MOTION_REGISTRY.FADE.IN).toConstantValue(fadeIn);
 MOTION_MODULE.bind(MOTION_REGISTRY.TEXT.DEFAULT).toConstantValue(
   defaultTextVariants,
 );
+MOTION_MODULE.bind(MOTION_REGISTRY.SLIDER).toConstantValue(slider);
 
 export const { lazyInject: injectMotion } = getDecorators(MOTION_MODULE);
