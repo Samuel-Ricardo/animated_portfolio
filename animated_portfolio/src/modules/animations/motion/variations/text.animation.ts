77b48ad0a362b8ea36fba9edd2f23436fd56ddf4
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 15:47:07 -0300

    [ :clapper: ] | create: animation - text > default (modules::motion)

---
+++ animated_portfolio/src/modules/animations/motion/variations/text.animation.ts
@@ -0,0 +1,24 @@
+import { MotionProps } from "framer-motion";
+
+export const defaultTextVariants: MotionProps | any = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
