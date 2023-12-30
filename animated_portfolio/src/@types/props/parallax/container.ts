5cb91c8b4695db1920ba979e41fc015f646361fd
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 22:37:04 -0300

    [ :label: ] | update: props - parallax (@types)

--- animated_portfolio/src/@types/props/parallax/container.ts
+++ animated_portfolio/src/@types/props/parallax/container.ts
@@ -1,4 +1,4 @@
 export interface IParallaxContainerProps extends React.PropsWithChildren {
   theme: "primary" | "secondary";
-  ref?: React.RefObject<HTMLSelectElement>;
+  target?: React.RefObject<HTMLSelectElement>;
 }
