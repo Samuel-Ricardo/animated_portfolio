c1ecad42dd8bea93bea25614e9a6127d7bfd6825
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 22:35:16 -0300

    [ :art: ] | update: component - hero > image (app)

--- animated_portfolio/src/components/hero/image/image.component.tsx
+++ animated_portfolio/src/components/hero/image/image.component.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 
 export const HeroImage = () => (
   <div className="perfil">
-    <Image src="/hero.png" alt="hero perfil image" width={720} height={1280} />
+    <Image
+      src="https://github.com/Samuel-Ricardo/BioMisa-Site/blob/master/biomisa-site/src/Images/Samuel.png?raw=true"
+      alt="hero perfil image"
+      width={720}
+      height={1280}
+    />
   </div>
 );
