09a04b786f56715aba29f7d879e6566f5141d13e
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-30 10:30:02 -0300

    [ :art: ] | update: component - services (app)

---
+++ animated_portfolio/src/components/services/header/header.component.tsx
@@ -0,0 +1,28 @@
+import { MotionB } from "@/components/motion/b.component";
+import { MotionDiv } from "@/components/motion/div.component";
+import Image from "next/image";
+
+export const ServicesHeader = () => {
+  return (
+    <MotionDiv className="header">
+      <div className="title">
+        <Image
+          src="/people.webp"
+          alt="peoples in meet"
+          width={100}
+          height={50}
+        />
+        <h1>
+          <MotionB whileHover={{ color: "orange" }}>Unique</MotionB> Ideas
+        </h1>
+      </div>
+      <div className="title">
+        <h1>
+          <MotionB whileHover={{ color: "orange" }}>For Your</MotionB>
+          Business.
+        </h1>
+        <button>WHAT WE DO?</button>
+      </div>
+    </MotionDiv>
+  );
+};
--- animated_portfolio/src/components/services/services.component.tsx
+++ animated_portfolio/src/components/services/services.component.tsx
@@ -1,8 +1,10 @@
 import { ServicesContainer } from "./container/container.component";
 import { Footnote } from "./footnote/footnote.component";
+import { ServicesHeader } from "./header/header.component";
 
 export const ServicesOverview = () => (
   <ServicesContainer>
     <Footnote />
+    <ServicesHeader />
   </ServicesContainer>
 );
