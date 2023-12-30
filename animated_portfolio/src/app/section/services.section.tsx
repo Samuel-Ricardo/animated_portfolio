c52c5f43d3c03f777bcaf5075d04f34fd0097ea5
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-30 10:28:21 -0300

    [ :art: ] | update: section - services (app)

--- animated_portfolio/src/app/section/services.section.tsx
+++ animated_portfolio/src/app/section/services.section.tsx
@@ -1,9 +1,13 @@
 import { Parallax } from "@/components/parallax/parallax.component";
+import { ServicesOverview } from "@/components/services/services.component";
 
 export const Services = () => {
   return (
-    <section id="Services">
-      <Parallax title="What We Do?" theme="primary" />
-    </section>
+    <>
+      <section id="Services">
+        <Parallax title="What We Do?" theme="primary" />
+      </section>
+      <ServicesOverview />
+    </>
   );
 };
