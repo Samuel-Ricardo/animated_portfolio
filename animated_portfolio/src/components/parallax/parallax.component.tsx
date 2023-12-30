fea0e76a77529355c1b7364ac85d818f87810040
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 22:13:56 -0300

    [ :art: ] | update: component - parallax (app)

--- animated_portfolio/src/components/parallax/parallax.component.tsx
+++ animated_portfolio/src/components/parallax/parallax.component.tsx
@@ -1,4 +1,5 @@
 "use client";
+import "./parallax.style.scss";
 
 import { IParallaxProps } from "@/@types/props/parallax";
 import { ParallaxContainer } from "@/components/parallax/container/container.component";
@@ -6,6 +7,7 @@ import { MotionH1 } from "../motion/h1.component";
 import { MotionDiv } from "../motion/div.component";
 import { useRef } from "react";
 import { useParallaxCoordinates } from "@/hooks/parallax/coordinates.hook";
+import Image from "next/image";
 
 export const Parallax = ({ theme, title }: IParallaxProps) => {
   const target = useRef(null);
@@ -14,17 +16,25 @@ export const Parallax = ({ theme, title }: IParallaxProps) => {
   return (
     <ParallaxContainer theme={theme} target={target}>
       <MotionH1 style={{ y: layer1 }}>{title}</MotionH1>
-      <MotionDiv className="mountains" />
-      <MotionDiv
-        className="planets"
-        style={{
-          y: layer2,
-          backgroundImage: `url(${
-            theme === "primary" ? "/planets.png" : "/sun.png"
-          })`,
-        }}
-      />
-      <MotionDiv style={{ x: layer2 }} className="stars" />
+      <MotionDiv className="mountains">
+        <Image
+          src="/mountains.png"
+          alt="mountains parallax background"
+          fill
+          quality={100}
+        />
+      </MotionDiv>
+      <MotionDiv className="planets" style={{ y: layer2 }}>
+        <Image
+          src={`${theme === "primary" ? "/planets.png" : "/sun.png"}`}
+          alt="planets"
+          fill
+          quality={100}
+        />
+      </MotionDiv>
+      <MotionDiv style={{ x: layer2 }} className="stars">
+        <Image src="/stars.png" alt="stars parallax" fill quality={100} />
+      </MotionDiv>
     </ParallaxContainer>
   );
 };
