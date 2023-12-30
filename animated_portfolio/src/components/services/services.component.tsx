3df524747bf6ae765cb0db74112a4130f1c21ef9
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 23:56:32 -0300

    [ :art: ] | update: component - services (app)

---
+++ animated_portfolio/src/components/services/services.component.tsx
@@ -0,0 +1,8 @@
+import { ServicesContainer } from "./container/container.component";
+import { Footnote } from "./footnote/footnote.component";
+
+export const ServicesOverview = () => (
+  <ServicesContainer>
+    <Footnote />
+  </ServicesContainer>
+);
