6ffbf49f5cd7223f4d76f7e1abba782440eed8ab
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 22:18:28 -0300

    [ :art: ] | create: section - portfolio (app)

---
+++ animated_portfolio/src/app/section/portfolio.section.tsx
@@ -0,0 +1,9 @@
+import { Parallax } from "@/components/parallax/parallax.component";
+
+export const Portfolio = () => {
+  return (
+    <section id="Portfolio">
+      <Parallax title="What We Did" theme="secondary" />
+    </section>
+  );
+};
