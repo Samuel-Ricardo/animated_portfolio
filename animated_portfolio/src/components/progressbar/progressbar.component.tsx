b5e746cbd89486fdda06c3ae51f6dbad567729d1
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-30 14:41:23 -0300

    [ :label: ] | create: props - progressbar  (@types)

--- animated_portfolio/src/components/progressbar/progressbar.component.tsx
+++ animated_portfolio/src/components/progressbar/progressbar.component.tsx
@@ -1,6 +1,7 @@
+import { IProgressBarProps } from "@/@types/props/progressbar";
 import { MotionDiv } from "@/components/motion/div.component";
 
-export const ProgressBar = ({ progress }: { progress: number }) => (
+export const ProgressBar = ({ progress }: IProgressBarProps) => (
   <div className="progress">
     <h1>Featured Works</h1>
     <MotionDiv style={{ scaleX: progress }} className="bar" />
