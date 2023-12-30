6af33ffd4206d1842981d575b9b76591d78ed96f
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-12-29 22:50:53 -0300

    [ :clapper: ] | create: animation - slide in (modules::motion)

---
+++ animated_portfolio/src/modules/animations/motion/variations/slideIn.animation.ts
@@ -0,0 +1,18 @@
+import { MotionProps } from "framer-motion";
+
+export const slideIn: MotionProps = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
