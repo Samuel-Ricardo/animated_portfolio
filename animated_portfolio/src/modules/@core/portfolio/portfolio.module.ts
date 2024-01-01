072503d5286ba4228be6077e6d5e497fa68799bb
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:39:59 -0300

    [ :syringe: ] | create: module - portfolio > controller (module::core)

--- animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
@@ -8,3 +8,4 @@ export const PORTFOLIO_MODULE = new Container({
 });
 
 PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.MAIN).to(PortfolioController);
+PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.CONTROLLER).to(PortfolioController);
