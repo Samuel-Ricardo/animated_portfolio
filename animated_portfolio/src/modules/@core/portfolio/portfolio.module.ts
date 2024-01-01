a1eb88ff19a267e0d69831ac9e4b9b4f2e3ae642
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:39:25 -0300

    [ :syringe: ] | create: module - portfolio (module::core)

---
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
@@ -0,0 +1,10 @@
+import { Container } from "inversify";
+import { PORTFOLIO_REGISTRY } from "./portfolio.registry";
+import { PortfolioController } from "./controller/portfolio.controller";
+
+export const PORTFOLIO_MODULE = new Container({
+  autoBindInjectable: true,
+  defaultScope: "Singleton",
+});
+
+PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.MAIN).to(PortfolioController);
