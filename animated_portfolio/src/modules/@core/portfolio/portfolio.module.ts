de3a203a83ec53ce02b2d2c0393f60e1d14becb8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:40:51 -0300

    [ :syringe: ] | create: module - portfolio > service (module::core)

--- animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
@@ -1,6 +1,7 @@
 import { Container } from "inversify";
 import { PORTFOLIO_REGISTRY } from "./portfolio.registry";
 import { PortfolioController } from "./controller/portfolio.controller";
+import { PortfolioService } from "./service/portfolio.service";
 
 export const PORTFOLIO_MODULE = new Container({
   autoBindInjectable: true,
@@ -9,3 +10,4 @@ export const PORTFOLIO_MODULE = new Container({
 
 PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.MAIN).to(PortfolioController);
 PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.CONTROLLER).to(PortfolioController);
+PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.SERVICE).to(PortfolioService);
