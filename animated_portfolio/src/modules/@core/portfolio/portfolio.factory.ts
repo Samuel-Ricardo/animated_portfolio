4b60de63ff92dc9c8adc4e14a0a8e64022ae6a68
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:45:18 -0300

    [ :syringe: ] | create: factory - portfolio > use-case (module::core)

--- animated_portfolio/src/modules/@core/portfolio/portfolio.factory.ts
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.factory.ts
@@ -2,6 +2,7 @@ import { PortfolioController } from "./controller/portfolio.controller";
 import { PORTFOLIO_MODULE } from "./portfolio.module";
 import { PORTFOLIO_REGISTRY } from "./portfolio.registry";
 import { PortfolioService } from "./service/portfolio.service";
+import { SelectAllPortfoliosUseCase } from "./use_case/select/all.use_case";
 
 export const PORTFOLIO_FACTORY = {
   MAIN: () =>
@@ -10,4 +11,12 @@ export const PORTFOLIO_FACTORY = {
     PORTFOLIO_MODULE.get<PortfolioController>(PORTFOLIO_REGISTRY.CONTROLLER),
   SERVICE: () =>
     PORTFOLIO_MODULE.get<PortfolioService>(PORTFOLIO_REGISTRY.SERVICE),
+  USE_CASE: {
+    SELECT: {
+      ALL: () =>
+        PORTFOLIO_MODULE.get<SelectAllPortfoliosUseCase>(
+          PORTFOLIO_REGISTRY.USE_CASE.SELECT.ALL,
+        ),
+    },
+  },
 };
