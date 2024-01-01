b6af227232f7783c312c5e7c4565741fae7cbb47
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:44:41 -0300

    [ :syringe: ] | create: factory - portfolio > service (module::core)

--- animated_portfolio/src/modules/@core/portfolio/portfolio.factory.ts
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.factory.ts
@@ -1,10 +1,13 @@
 import { PortfolioController } from "./controller/portfolio.controller";
 import { PORTFOLIO_MODULE } from "./portfolio.module";
 import { PORTFOLIO_REGISTRY } from "./portfolio.registry";
+import { PortfolioService } from "./service/portfolio.service";
 
 export const PORTFOLIO_FACTORY = {
   MAIN: () =>
     PORTFOLIO_MODULE.get<PortfolioController>(PORTFOLIO_REGISTRY.MAIN),
   CONTROLLER: () =>
     PORTFOLIO_MODULE.get<PortfolioController>(PORTFOLIO_REGISTRY.CONTROLLER),
+  SERVICE: () =>
+    PORTFOLIO_MODULE.get<PortfolioService>(PORTFOLIO_REGISTRY.SERVICE),
 };
