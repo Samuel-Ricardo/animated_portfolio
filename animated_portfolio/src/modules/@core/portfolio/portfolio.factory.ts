6437342216a65153543d2f1d304dd1377fa86797
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:43:45 -0300

    [ :syringe: ] | create: factory - portfolio (module::core)

---
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.factory.ts
@@ -0,0 +1,8 @@
+import { PortfolioController } from "./controller/portfolio.controller";
+import { PORTFOLIO_MODULE } from "./portfolio.module";
+import { PORTFOLIO_REGISTRY } from "./portfolio.registry";
+
+export const PORTFOLIO_FACTORY = {
+  MAIN: () =>
+    PORTFOLIO_MODULE.get<PortfolioController>(PORTFOLIO_REGISTRY.MAIN),
+};
