96df44b9beb9b078ae8bdea6b5551ea6f72e8ef7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:44:16 -0300

    [ :syringe: ] | create: factory - portfolio > controller (module::core)

--- animated_portfolio/src/modules/@core/portfolio/portfolio.factory.ts
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.factory.ts
@@ -5,4 +5,6 @@ import { PORTFOLIO_REGISTRY } from "./portfolio.registry";
 export const PORTFOLIO_FACTORY = {
   MAIN: () =>
     PORTFOLIO_MODULE.get<PortfolioController>(PORTFOLIO_REGISTRY.MAIN),
+  CONTROLLER: () =>
+    PORTFOLIO_MODULE.get<PortfolioController>(PORTFOLIO_REGISTRY.CONTROLLER),
 };
