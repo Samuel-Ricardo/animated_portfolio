2c55e91eb2a0cb0565b6f6f17f0b7eb358a4fcb1
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:41:37 -0300

    [ :syringe: ] | create: module - portfolio > use_case (module::core)

--- animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
@@ -2,6 +2,7 @@ import { Container } from "inversify";
 import { PORTFOLIO_REGISTRY } from "./portfolio.registry";
 import { PortfolioController } from "./controller/portfolio.controller";
 import { PortfolioService } from "./service/portfolio.service";
+import { SelectAllPortfoliosUseCase } from "./use_case/select/all.use_case";
 
 export const PORTFOLIO_MODULE = new Container({
   autoBindInjectable: true,
@@ -11,3 +12,7 @@ export const PORTFOLIO_MODULE = new Container({
 PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.MAIN).to(PortfolioController);
 PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.CONTROLLER).to(PortfolioController);
 PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.SERVICE).to(PortfolioService);
+
+PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.USE_CASE.SELECT.ALL).to(
+  SelectAllPortfoliosUseCase,
+);
