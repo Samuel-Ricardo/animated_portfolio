86096204ac2e4e6fc242a10a57c0fe8b6e098495
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 17:49:35 -0300

    [ :syringe: ] | create: lazy - module > portfolio (module::core)

--- animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
+++ animated_portfolio/src/modules/@core/portfolio/portfolio.module.ts
@@ -3,6 +3,7 @@ import { PORTFOLIO_REGISTRY } from "./portfolio.registry";
 import { PortfolioController } from "./controller/portfolio.controller";
 import { PortfolioService } from "./service/portfolio.service";
 import { SelectAllPortfoliosUseCase } from "./use_case/select/all.use_case";
+import getDecorators from "inversify-inject-decorators";
 
 export const PORTFOLIO_MODULE = new Container({
   autoBindInjectable: true,
@@ -16,3 +17,5 @@ PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.SERVICE).to(PortfolioService);
 PORTFOLIO_MODULE.bind(PORTFOLIO_REGISTRY.USE_CASE.SELECT.ALL).to(
   SelectAllPortfoliosUseCase,
 );
+
+export const { lazyInject: injectPortfolio } = getDecorators(PORTFOLIO_MODULE);
