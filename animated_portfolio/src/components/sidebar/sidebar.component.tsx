2db22bca2151bc61a66b7959b9f5fa9f3cb618c5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 11:36:19 -0300

    [ :art: ] | update: componenet - sidebar (app)

--- animated_portfolio/src/components/sidebar/sidebar.component.tsx
+++ animated_portfolio/src/components/sidebar/sidebar.component.tsx
@@ -2,9 +2,14 @@
 
 import { useState } from "react";
 import { SidebarContainer } from "./container/container.component";
+import { SidebarLinks } from "./links/links.component";
 
 export const Sidebar = () => {
   const [open, setOpen] = useState(false);
 
-  return <SidebarContainer open={open}>Hello World!</SidebarContainer>;
+  return (
+    <SidebarContainer open={open}>
+      <SidebarLinks />
+    </SidebarContainer>
+  );
 };
