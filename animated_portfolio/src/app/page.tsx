d711a0feea01287bc01060decab75b116cf95018
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 15:47:31 -0300

    [ :art: ] | update: page - home > contact (app)

--- animated_portfolio/src/app/page.tsx
+++ animated_portfolio/src/app/page.tsx
@@ -2,6 +2,7 @@ import { Cursor } from "@/components/cursor/cursor.component";
 import { Hero } from "./section/hero.section";
 import { Services } from "./section/services.section";
 import { Portfolio } from "./section/portfolio.section";
+import { Contact } from "./section/contact.section";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
       </section>
       <Services />
       <Portfolio />
-      <section id="Contact">Contact</section>
+      <Contact />
     </main>
   );
 }
