786d0cc09d496804fac506066eee0e7fe234d44f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 10:39:14 -0300

    [ :clapper: ] | animate: portfolio - overview (app::component)

--- animated_portfolio/src/components/portfolio/item/item.component.tsx
+++ animated_portfolio/src/components/portfolio/item/item.component.tsx
@@ -1,13 +1,21 @@
+"use client";
+
 import { PortfolioItemConteiner } from "./container/container.component";
 import { PortfolioItemPreview } from "./preview/preview.component";
 import { PortfolioItemDetails } from "./details/details.component";
 import { IPortfolioDTO } from "@/modules/@core/portfolio/DTO/portfolio.dto";
+import { usePortfolioItemScroll } from "@/hooks/scroll/portfolio/item/scroll.hook";
 
 export const PortfolioItem = ({ title, description, image }: IPortfolioDTO) => {
+  const { target, scrollY } = usePortfolioItemScroll();
   return (
     <PortfolioItemConteiner>
-      <PortfolioItemPreview src={image} />
-      <PortfolioItemDetails title={title} description={description} />
+      <PortfolioItemPreview src={image} target={target} />
+      <PortfolioItemDetails
+        title={title}
+        description={description}
+        position={scrollY}
+      />
     </PortfolioItemConteiner>
   );
 };
