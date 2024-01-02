c744daecf7d606dea72f4b4083071a5f191ccbf9
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 09:33:31 -0300

    [ :syringe: ] | inject: repository - local > portfolio (module::core:use-case)

--- animated_portfolio/src/modules/@core/portfolio/use_case/select/all.use_case.ts
+++ animated_portfolio/src/modules/@core/portfolio/use_case/select/all.use_case.ts
@@ -1,9 +1,13 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { type IPortfolioRepository } from "../../repository/portfolio.repository";
+import { MODULE } from "@/modules/app.registry";
 
 @injectable()
 export class SelectAllPortfoliosUseCase {
-  constructor(private repository: IPortfolioRepository) {}
+  constructor(
+    @inject(MODULE.REPOSITORY.LOCAL.PORTFOLIO)
+    private repository: IPortfolioRepository,
+  ) {}
 
   public execute() {
     return this.repository.selectAll();
