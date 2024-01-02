616fc2e941f0c555eaf2f5b837bf53bf1033a64a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 15:46:36 -0300

    [ :art: ] | create: section - contact (app)

---
+++ animated_portfolio/src/app/section/contact.section.tsx
@@ -0,0 +1,9 @@
+import { ContactArea } from "@/components/contact/contact.component";
+
+export const Contact = () => {
+  return (
+    <section id="Contact">
+      <ContactArea />
+    </section>
+  );
+};
