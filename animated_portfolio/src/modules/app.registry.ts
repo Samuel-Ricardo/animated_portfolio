121834b266bcd71239afa7b3c506cdf86e1ea7a3
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 09:25:51 -0300

    [ :syringe: ] | update: registry - app > repository (module)

--- animated_portfolio/src/modules/app.registry.ts
+++ animated_portfolio/src/modules/app.registry.ts
@@ -1,7 +1,9 @@
 import { ANIMATION_REGISTRY as ANIMATION } from "./animations/animation.registry";
 import { PORTFOLIO_REGISTRY as PORTFOLIO } from "./@core/portfolio/portfolio.registry";
+import { REPOSITORY_REGISTRY as REPOSITORY } from "./repository/repository.registry";
 
 export const MODULE = {
   ANIMATION,
   PORTFOLIO,
+  REPOSITORY,
 };
