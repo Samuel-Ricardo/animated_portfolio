6ee0e943a89a2320b2239d134871801c413f8ff6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 09:27:50 -0300

    [ :syringe: ] | update: factory - app > repository (module)

--- animated_portfolio/src/modules/app.factory.ts
+++ animated_portfolio/src/modules/app.factory.ts
@@ -1,7 +1,9 @@
 import { PORTFOLIO_FACTORY as PORTFOLIO } from "./@core/portfolio/portfolio.factory";
 import { ANIMATION_FACTORY as ANIMATION } from "./animations/animation.factory";
+import { REPOSITORY_FACTORY as REPOSITORY } from "./repository/repository.factory";
 
 export const MODULES = {
   ANIMATION,
   PORTFOLIO,
+  REPOSITORY,
 };
