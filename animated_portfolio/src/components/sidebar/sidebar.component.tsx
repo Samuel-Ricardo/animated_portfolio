7a16a70c9d33f373d4c59ced2fd8b4198c9da7c4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 14:51:56 -0300

    [ :art: ] | update: component - sidebar (app)

--- animated_portfolio/src/components/sidebar/sidebar.component.tsx
+++ animated_portfolio/src/components/sidebar/sidebar.component.tsx
@@ -3,13 +3,14 @@
 import { useState } from "react";
 import { SidebarContainer } from "./container/container.component";
 import { SidebarLinks } from "./links/links.component";
+import { ToggleButton } from "./toggle/button.component";
 
 export const Sidebar = () => {
   const [open, setOpen] = useState(false);
-
   return (
     <SidebarContainer open={open}>
       <SidebarLinks />
+      <ToggleButton setOpen={setOpen} />
     </SidebarContainer>
   );
 };
