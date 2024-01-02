8fee841303be6860235d3999f373d62ca174852c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 22:22:40 -0300

    [ :art: ] | update: componenet - portfolio > container (app)

--- animated_portfolio/src/components/portfolio/container/container.component.tsx
+++ animated_portfolio/src/components/portfolio/container/container.component.tsx
@@ -1,13 +1,12 @@
 import "./container.style.scss";
 
-import { PropsWithChildren, RefObject } from "react";
 import { ProgressBar } from "@/components/progressbar/progressbar.component";
 import { IPortfolioContainerProps } from "@/@types/props/portfolio/container";
 
 export const PortfolioContainer = ({
   children,
   target,
-  progress,
+  progress = 0.5,
 }: IPortfolioContainerProps) => {
   return (
     <div id="_portfolio" className="overview" ref={target}>
