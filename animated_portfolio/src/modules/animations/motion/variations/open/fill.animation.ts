7ddc676b8b6c153471b81847ff71a4e72e264e1e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 11:11:19 -0300

    [ :clapper: ] | create: animation - open > fill (modules::motion)

---
+++ animated_portfolio/src/modules/animations/motion/variations/open/fill.animation.ts
@@ -0,0 +1,12 @@
+import { TargetAndTransition } from "framer-motion";
+
+export const OPEN_FILL_ANIMATION = {
+  open: {
+    clipPath: "circle(1200px at 50px 50px)",
+    transition: { type: "spring", stiffness: 20 },
+  } as TargetAndTransition,
+  closed: {
+    clipPath: "circle(30px ar 50px 50px)",
+    transition: { type: "spring", delay: 0.5, stiffness: 400, damping: 40 },
+  } as TargetAndTransition,
+};
