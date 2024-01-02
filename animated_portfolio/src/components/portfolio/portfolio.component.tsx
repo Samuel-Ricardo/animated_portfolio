b45ca087f052ab87deabd90fe45aca5ee126c35d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 10:23:19 -0300

    [ :art: ] | update: componenet - portfolio (app)

--- animated_portfolio/src/components/portfolio/portfolio.component.tsx
+++ animated_portfolio/src/components/portfolio/portfolio.component.tsx
@@ -1,14 +1,10 @@
 import { PortfolioContainer } from "./container/container.component";
-import { PortfolioItem } from "./item/item.component";
+import { PortfolioItemsLoader } from "./loader/item.loader";
 
 export const PortfolioOverview = () => {
   return (
     <PortfolioContainer>
-      <PortfolioItem
-        title="Hello"
-        description="World"
-        image="https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-      />
+      <PortfolioItemsLoader />
     </PortfolioContainer>
   );
 };
