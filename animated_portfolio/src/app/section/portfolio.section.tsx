d27bec1b1bbec1a0d615f880705f02ce756ab357
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-01 22:20:32 -0300

    [ :art: ] | update: section - portfolio (app)

--- animated_portfolio/src/app/section/portfolio.section.tsx
+++ animated_portfolio/src/app/section/portfolio.section.tsx
@@ -1,9 +1,13 @@
 import { Parallax } from "@/components/parallax/parallax.component";
+import { PortfolioOverview } from "@/components/portfolio/portfolio.component";
 
 export const Portfolio = () => {
   return (
-    <section id="Portfolio">
-      <Parallax title="What We Did" theme="secondary" />
-    </section>
+    <>
+      <section id="Portfolio">
+        <Parallax title="What We Did" theme="secondary" />
+      </section>
+      <PortfolioOverview />
+    </>
   );
 };
