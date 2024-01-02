6902711a57d8806c2ea951f2eff96a39cf609460
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 16:22:20 -0300

    [ :art: ] | create: contact - details (app::components)

--- animated_portfolio/src/components/contact/details/details.component.tsx
+++ animated_portfolio/src/components/contact/details/details.component.tsx
@@ -1,10 +1,14 @@
 import { MotionDiv } from "@/components/motion/div.component";
 import { MotionH1 } from "@/components/motion/h1.component";
 import { ContactAreaDetailsItem } from "./item/item.component";
+import { MODULES } from "@/modules/app.factory";
 
 export const ContactAreaDetails = () => {
   return (
-    <MotionDiv>
+    <MotionDiv
+      className="details"
+      {...MODULES.ANIMATION.MOTION.SIMPLE.EMERGE()}
+    >
       <MotionH1>Let&#39s Work Together</MotionH1>
       <ContactAreaDetailsItem
         title="Mail"
