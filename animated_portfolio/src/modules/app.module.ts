12947409d2d933fbe5e6623e3d6f60f87dc4a7f6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 09:27:11 -0300

    [ :syringe: ] | update: module - app > repository (module)

--- animated_portfolio/src/modules/app.module.ts
+++ animated_portfolio/src/modules/app.module.ts
@@ -3,6 +3,7 @@
 import { Container } from "inversify";
 import { ANIMATION_MODULE } from "./animations/animation.module";
 import { PORTFOLIO_MODULE } from "./@core/portfolio/portfolio.module";
+import { REPOSITORY_MODULE } from "./repository/repository.module";
 
 const MODULE = new Container({ autoBindInjectable: true });
 
@@ -10,4 +11,5 @@ export const APP_MODULE = Container.merge(
   MODULE,
   ANIMATION_MODULE,
   PORTFOLIO_MODULE,
+  REPOSITORY_MODULE,
 );
--- animated_portfolio/src/modules/repository/repository.module.ts
+++ animated_portfolio/src/modules/repository/repository.module.ts
@@ -3,4 +3,7 @@ import { LOCAL_REPOSITORY_MODULE } from "./local/local.module";
 
 const MODULE = new Container({ autoBindInjectable: true });
 
-const REPOSITORY_MODULE = Container.merge(MODULE, LOCAL_REPOSITORY_MODULE);
+export const REPOSITORY_MODULE = Container.merge(
+  MODULE,
+  LOCAL_REPOSITORY_MODULE,
+);
