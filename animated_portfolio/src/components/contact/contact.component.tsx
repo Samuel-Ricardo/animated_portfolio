e3348fa696ee71076aa08f5adfb75fd017c1ec25
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 15:45:43 -0300

    [ :art: ] | create: component - contact (app)

---
+++ animated_portfolio/src/components/contact/contact.component.tsx
@@ -0,0 +1,5 @@
+import { ContactAreaContainer } from "./container/container.component";
+
+export const ContactArea = () => {
+  return <ContactAreaContainer>Hello World! :D</ContactAreaContainer>;
+};
