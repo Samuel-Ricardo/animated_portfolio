90d9fe03f4b4cb3018d9ed127f5822c73d66faf6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 14:13:57 -0300

    [ :clapper: ] | update: module - motion > drawn (modules::animation)

--- animated_portfolio/src/modules/animations/motion/motion.module.ts
+++ animated_portfolio/src/modules/animations/motion/motion.module.ts
@@ -7,6 +7,7 @@ import { slider } from "./variations/slider.animation";
 import { slideIn } from "./variations/slideIn.animation";
 import { OPEN_FILL_ANIMATION } from "./variations/open/fill.animation";
 import { EMERGE_ANIMATION } from "./variations/emerge.animation";
+import { DRAWN_ANIMATION } from "./variations/svg/drawn.animation";
 
 export const MOTION_MODULE = new Container({ defaultScope: "Singleton" });
 
@@ -20,5 +21,6 @@ MOTION_MODULE.bind(MOTION_REGISTRY.OPEN.FILL).toConstantValue(
   OPEN_FILL_ANIMATION,
 );
 MOTION_MODULE.bind(MOTION_REGISTRY.EMERGE).toConstantValue(EMERGE_ANIMATION);
+MOTION_MODULE.bind(MOTION_REGISTRY.SVG.DRAWN).toConstantValue(DRAWN_ANIMATION);
 
 export const { lazyInject: injectMotion } = getDecorators(MOTION_MODULE);
