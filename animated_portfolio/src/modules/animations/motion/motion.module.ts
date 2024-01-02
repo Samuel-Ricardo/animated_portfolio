d20a818cf1530ead237e805bb6cafec12102f8d8
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 11:13:06 -0300

    [ :clapper: ] | update: module - motion > open fill (modules::animation)

--- animated_portfolio/src/modules/animations/motion/motion.module.ts
+++ animated_portfolio/src/modules/animations/motion/motion.module.ts
@@ -5,6 +5,7 @@ import getDecorators from "inversify-inject-decorators";
 import { defaultTextVariants } from "./variations/text.animation";
 import { slider } from "./variations/slider.animation";
 import { slideIn } from "./variations/slideIn.animation";
+import { OPEN_FILL_ANIMATION } from "./variations/open/fill.animation";
 
 export const MOTION_MODULE = new Container({ defaultScope: "Singleton" });
 
@@ -14,5 +15,8 @@ MOTION_MODULE.bind(MOTION_REGISTRY.TEXT.DEFAULT).toConstantValue(
 );
 MOTION_MODULE.bind(MOTION_REGISTRY.SLIDER).toConstantValue(slider);
 MOTION_MODULE.bind(MOTION_REGISTRY.SLIDE.IN).toConstantValue(slideIn);
+MOTION_MODULE.bind(MOTION_REGISTRY.OPEN.FILL).toConstantValue(
+  OPEN_FILL_ANIMATION,
+);
 
 export const { lazyInject: injectMotion } = getDecorators(MOTION_MODULE);
