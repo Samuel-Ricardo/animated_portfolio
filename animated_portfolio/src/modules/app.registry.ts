b0f30e1647372bbdf2e6bc7e086e3fae6ffd0d8e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 22:38:54 -0300

    [ :syringe: ] | update: registry - app > validation (module)

--- animated_portfolio/src/modules/app.registry.ts
+++ animated_portfolio/src/modules/app.registry.ts
@@ -1,9 +1,11 @@
 import { ANIMATION_REGISTRY as ANIMATION } from "./animations/animation.registry";
 import { PORTFOLIO_REGISTRY as PORTFOLIO } from "./@core/portfolio/portfolio.registry";
 import { REPOSITORY_REGISTRY as REPOSITORY } from "./repository/repository.registry";
+import { VALIDATION_ZOD_REGISTRY as VALIDATION } from "./validation/zod/zod.registry";
 
 export const MODULE = {
   ANIMATION,
   PORTFOLIO,
   REPOSITORY,
+  VALIDATION,
 };
