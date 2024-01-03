3b6ce2d4d7228d256111bfcf580cde4c7658ce2b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 23:11:25 -0300

    [ :art: ] | update: contact - form > body (app::component)

--- animated_portfolio/src/components/contact/form/body/body.component.tsx
+++ animated_portfolio/src/components/contact/form/body/body.component.tsx
@@ -1,15 +1,34 @@
+"use client";
 import "./body.style.scss";
 
 import { MotionForm } from "@/components/motion/form/form.component";
 import { MODULES } from "@/modules/app.factory";
+import { ContactFormData } from "@/modules/validation/zod/form/contact.validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
 
 export const ContactFormBody = () => {
   const animation = MODULES.ANIMATION.MOTION.OPACITY()({});
+  const validation = MODULES.VALIDATION.ZOD.CONTACT.FORM();
+
+  const { register, handleSubmit, setValue } = useForm<ContactFormData>({
+    resolver: zodResolver(validation),
+  });
+
+  const submit = useCallback(
+    () =>
+      handleSubmit(async (data) => {
+        console.log({ data });
+      }),
+    [handleSubmit],
+  );
+
   return (
-    <MotionForm {...animation}>
-      <input type="text" required placeholder="Name" name="name" />
-      <input type="email" required placeholder="Email" name="email" />
-      <textarea rows={8} placeholder="Message" name="message" />
+    <MotionForm {...animation} onSubmit={submit()}>
+      <input type="text" required placeholder="Name" {...register("name")} />
+      <input type="email" required placeholder="Email" {...register("email")} />
+      <textarea rows={8} placeholder="Message" {...register("message")} />
       <button>Submit</button>
     </MotionForm>
   );
