d91c6e147ea57d230ad4b6d635c73d969ed69609
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 21:18:51 -0300

    [ :art: ] | create: contact - form > body (app::component)

---
+++ animated_portfolio/src/components/contact/form/body/body.component.tsx
@@ -0,0 +1,14 @@
+import { MotionForm } from "@/components/motion/form/form.component";
+import { MODULES } from "@/modules/app.factory";
+
+export const ContactFormBody = () => {
+  const animation = MODULES.ANIMATION.MOTION.OPACITY()({});
+  return (
+    <MotionForm {...animation}>
+      <input type="text" required placeholder="Name" name="name" />
+      <input type="email" required placeholder="Email" name="email" />
+      <textarea rows={8} placeholder="Message" name="message" />
+      <button>Submit</button>
+    </MotionForm>
+  );
+};
--- animated_portfolio/src/components/contact/form/form.component.tsx
+++ animated_portfolio/src/components/contact/form/form.component.tsx
@@ -1,10 +1,12 @@
 import { ContactAreaAnimation } from "./animation/animation.component";
+import { ContactFormBody } from "./body/body.component";
 import { ContactAreaFormContainer } from "./container/container.component";
 
 export const ContactForm = () => {
   return (
     <ContactAreaFormContainer>
       <ContactAreaAnimation />
+      <ContactFormBody />
     </ContactAreaFormContainer>
   );
 };
