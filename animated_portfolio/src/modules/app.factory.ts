b2edeb4d8031de29225d77de2e1284c1d132993c
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-02 22:39:44 -0300

    [ :syringe: ] | update: factory - app > validation (module)

--- animated_portfolio/src/modules/app.factory.ts
+++ animated_portfolio/src/modules/app.factory.ts
@@ -1,9 +1,11 @@
 import { PORTFOLIO_FACTORY as PORTFOLIO } from "./@core/portfolio/portfolio.factory";
 import { ANIMATION_FACTORY as ANIMATION } from "./animations/animation.factory";
 import { REPOSITORY_FACTORY as REPOSITORY } from "./repository/repository.factory";
+import { VALIDATION_FACTORY as VALIDATION } from "./validation/validation.factory";
 
 export const MODULES = {
   ANIMATION,
   PORTFOLIO,
   REPOSITORY,
+  VALIDATION,
 };
