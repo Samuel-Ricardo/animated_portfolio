c8d1ed76096327514a6f2ea00ce7da2332faf9de
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-04 11:05:29 -0300

    [ :syringe: ] | update: registry - config (module)

--- animated_portfolio/src/modules/@config/config.registry.ts
+++ animated_portfolio/src/modules/@config/config.registry.ts
@@ -3,4 +3,5 @@ import { CONFIG } from "./configs/config.global";
 export const CONFIG_REGISTRY = {
   ...CONFIG,
   ALL: Symbol.for("MODULES::CONFIG:ALL"),
+  ENV: Symbol.for("MODULES::CONFIG:ENV"),
 };
