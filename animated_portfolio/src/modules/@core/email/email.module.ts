9a014fdf67d053f4b4b9dd1309785bc4f4540227
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-04 18:10:03 -0300

    [ :syringe: ] | update: module - email (module::core)

--- animated_portfolio/src/modules/@core/email/email.module.ts
+++ animated_portfolio/src/modules/@core/email/email.module.ts
@@ -1,6 +1,10 @@
 import { EMAILJS_GATEWAY_MODULE } from "@/modules/gateway/emailjs/emailjs.module";
 import { Container } from "inversify";
 import getDecorators from "inversify-inject-decorators";
+import { EMAIL_REGISTRY } from "./email.registry";
+import { EmailController } from "./controller/email.controller";
+import { EmailService } from "./service/email.service";
+import { SendEmailUseCase } from "./use_case/send.use_case";
 
 const MODULE = new Container({
   autoBindInjectable: true,
@@ -8,5 +12,10 @@ const MODULE = new Container({
 });
 
 export const EMAIL_MODULE = Container.merge(MODULE, EMAILJS_GATEWAY_MODULE);
+
+EMAIL_MODULE.bind(EMAIL_REGISTRY.MAIN).to(EmailController);
+EMAIL_MODULE.bind(EMAIL_REGISTRY.CONTROLLER).to(EmailController);
+EMAIL_MODULE.bind(EMAIL_REGISTRY.SERVICE).to(EmailService);
+EMAIL_MODULE.bind(EMAIL_REGISTRY.USE_CASE.SEND).to(SendEmailUseCase);
 
 export const { lazyInject: injectEmail } = getDecorators(EMAIL_MODULE);
