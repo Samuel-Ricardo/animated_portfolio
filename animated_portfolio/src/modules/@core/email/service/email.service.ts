003f428314b1349c9ed418f79dafa0be8fc6e3ea
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-04 17:21:37 -0300

    [ :sparkles: ] | update: service - email (module::core)

--- animated_portfolio/src/modules/@core/email/service/email.service.ts
+++ animated_portfolio/src/modules/@core/email/service/email.service.ts
@@ -1,13 +1,12 @@
 import { injectable } from "inversify";
 import { SendEmailUseCase } from "../use_case/send.use_case";
-import { EmailDTO } from "../dto/email.dto";
 import { Email } from "../entity/email.entity";
 
 @injectable()
 export class EmailService {
   constructor(protected sendEmail: SendEmailUseCase) {}
 
-  async send(email: EmailDTO) {
-    return await this.sendEmail.execute(Email.fromDTO(email));
+  async send(email: Email) {
+    return await this.sendEmail.execute(email);
   }
 }
