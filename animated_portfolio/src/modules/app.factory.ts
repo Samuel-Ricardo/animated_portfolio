341e79afb9999b2b6c8bd5a2711037e083dac39b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-04 11:18:51 -0300

    [ :syringe: ] | update: factory - app > mailer (module)

--- animated_portfolio/src/modules/app.factory.ts
+++ animated_portfolio/src/modules/app.factory.ts
@@ -1,6 +1,7 @@
 import { CONFIG_FACTORY as CONFIG } from "./@config/config.factory";
 import { PORTFOLIO_FACTORY as PORTFOLIO } from "./@core/portfolio/portfolio.factory";
 import { ANIMATION_FACTORY as ANIMATION } from "./animations/animation.factory";
+import { MAILER_FACTORY as MAILER } from "./mailer/mailer.factory";
 import { REPOSITORY_FACTORY as REPOSITORY } from "./repository/repository.factory";
 import { VALIDATION_FACTORY as VALIDATION } from "./validation/validation.factory";
 
@@ -10,4 +11,5 @@ export const MODULES = {
   REPOSITORY,
   VALIDATION,
   CONFIG,
+  MAILER,
 };
