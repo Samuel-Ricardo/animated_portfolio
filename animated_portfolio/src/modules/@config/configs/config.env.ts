72b02d168d5a252a5d9a8f4b58a51a1a7e3d8065
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-04 14:13:00 -0300

    [ :closed_lock_with_key: ] | update: config - env (config::enviroment)

--- animated_portfolio/src/modules/@config/configs/config.env.ts
+++ animated_portfolio/src/modules/@config/configs/config.env.ts
@@ -2,7 +2,13 @@ export const ENV = {
   ...process.env,
   MAILER: {
     KEY: {
-      PUBLIC: process.env.NEXT_PUBLIC_MAILER_PUBLIC_KEY,
+      PUBLIC: process.env.NEXT_PUBLIC_MAILER_PUBLIC_KEY || "",
+    },
+    SERVICE: {
+      ID: process.env.NEXT_PUBLIC_MAILER_SERVICE_ID || "",
+    },
+    TEMPLATE: {
+      ID: process.env.NEXT_PUBLIC_MAILER_TEMPLATE_ID || "",
     },
   },
 };
