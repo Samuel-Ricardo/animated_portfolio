d5fd79d92418a9c04d097de5d90681c1b954fb6b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-04 11:29:05 -0300

    [ :label: ] | create: entity - email > toDTO (module::core)

--- animated_portfolio/src/modules/@core/email/entity/email.entity.ts
+++ animated_portfolio/src/modules/@core/email/entity/email.entity.ts
@@ -1,7 +1,17 @@
+import { EmailDTO } from "../dto/email.dto";
+
 export class Email {
   constructor(
     public to_name: string,
     public from_name: string,
     public message_html: string,
   ) {}
+
+  toDTO(): EmailDTO {
+    return {
+      to_name: this.to_name,
+      from_name: this.from_name,
+      message_html: this.message_html,
+    };
+  }
 }
