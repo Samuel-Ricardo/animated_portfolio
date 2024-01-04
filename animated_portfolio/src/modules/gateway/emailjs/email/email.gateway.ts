500f146f6215a9718bbf96227fd32248e75b3bce
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-04 14:17:05 -0300

    [ :door: ] | implements: gateway - email (module::emailjs)

--- animated_portfolio/src/modules/gateway/emailjs/email/email.gateway.ts
+++ animated_portfolio/src/modules/gateway/emailjs/email/email.gateway.ts
@@ -1,10 +1,24 @@
+import { type Environment } from "@/@types/modules/config/env";
+import { type EmailJS } from "@/@types/modules/mailer/emailjs";
 import { Email } from "@/modules/@core/email/entity/email.entity";
 import { EmailGateway } from "@/modules/@core/email/gateway/email.gateway";
-import { injectable } from "inversify";
+import { MODULE } from "@/modules/app.registry";
+import { inject, injectable } from "inversify";
 
 @injectable()
 export class EmailEmailJSGateway implements EmailGateway {
-  send(email: Email): Promise<void> {
-    throw new Error("Method not implemented.");
+  constructor(
+    @inject(MODULE.MAILER.EMAILJS)
+    protected mailer: EmailJS,
+    @inject(MODULE.CONFIG.ENV)
+    protected ENV: Environment,
+  ) {}
+
+  async send(email: Email): Promise<any> {
+    return await this.mailer.send(
+      this.ENV.MAILER.SERVICE.ID,
+      this.ENV.MAILER.TEMPLATE.ID,
+      { ...email.toDTO() },
+    );
   }
 }
