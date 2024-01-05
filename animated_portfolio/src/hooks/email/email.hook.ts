0e4571509be5f4e42d0c4efd49efd0863f76acc5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-05 08:00:27 -0300

    [ :link: ] | update: hook - email > toast (app)

--- animated_portfolio/src/hooks/email/email.hook.ts
+++ animated_portfolio/src/hooks/email/email.hook.ts
@@ -1,12 +1,18 @@
 import { EmailDTO } from "@/modules/@core/email/dto/email.dto";
 import { MODULES } from "@/modules/app.factory";
 import { useCallback, useMemo } from "react";
+import toast from "react-hot-toast";
 
 export const useEmail = () => {
   const mailer = useMemo(() => MODULES.EMAIL.MAIN(), []);
 
   const sendEmail = useCallback(
-    (email: EmailDTO) => mailer.sendEmail(email),
+    async (email: EmailDTO) =>
+      await toast.promise(mailer.sendEmail(email), {
+        loading: "🌱 | Sending email...",
+        success: "📨 | Email sent!",
+        error: "🥀 | Error sending email.",
+      }),
     [mailer],
   );
 
