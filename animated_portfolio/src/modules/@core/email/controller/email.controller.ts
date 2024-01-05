60f0fa6f5d07d9204340b0dc44727f6c941e1b36
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-05 08:14:00 -0300

    [ :syringe: ] | inject: service => controller (module::email)

--- animated_portfolio/src/modules/@core/email/controller/email.controller.ts
+++ animated_portfolio/src/modules/@core/email/controller/email.controller.ts
@@ -1,11 +1,15 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { EmailService } from "../service/email.service";
 import { EmailDTO } from "../dto/email.dto";
 import { Email } from "../entity/email.entity";
+import { MODULE } from "@/modules/app.registry";
 
 @injectable()
 export class EmailController {
-  constructor(protected service: EmailService) {}
+  constructor(
+    @inject(MODULE.EMAIL.SERVICE)
+    protected service: EmailService,
+  ) {}
 
   async sendEmail(email: EmailDTO) {
     return await this.service.send(Email.fromDTO(email));
