1812fae1a36ff3863e66faf43a897fca630fd43b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-05 08:16:18 -0300

    [ :syringe: ] | inject: use-case -> service (module::email)

--- animated_portfolio/src/modules/@core/email/service/email.service.ts
+++ animated_portfolio/src/modules/@core/email/service/email.service.ts
@@ -1,10 +1,14 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 import { SendEmailUseCase } from "../use_case/send.use_case";
 import { Email } from "../entity/email.entity";
+import { MODULE } from "@/modules/app.registry";
 
 @injectable()
 export class EmailService {
-  constructor(protected sendEmail: SendEmailUseCase) {}
+  constructor(
+    @inject(MODULE.EMAIL.USE_CASE.SEND)
+    protected sendEmail: SendEmailUseCase,
+  ) {}
 
   async send(email: Email) {
     return await this.sendEmail.execute(email);
